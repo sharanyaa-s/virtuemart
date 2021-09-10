8e24966f25ecf8813f5242020527d324559f5c64
sharanyaa-s <89771028+sharanyaa-s@users.noreply.github.com>
2021-09-10 13:58:45 +0530

    Delete virtueMartDemo directory

--- virtueMartDemo/src/main/java/com/vmd/utilities/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.vmd.utilities;
-
-public class Constants {
-	public static final String userName = "demo";
-	public static final String password = "demo";
-	public static final String productName = "cowboy Hat";
-	
-
-}
